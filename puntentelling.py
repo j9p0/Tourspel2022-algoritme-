7189b7a323d2b9e5a12b37e11dcdbb165d00562f
j9p0 <jellepieter@ypma.org>
2022-07-10 16:14:58 +0200

    Create puntentelling.py

---
+++ puntentelling.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Protest het algoritme voor het beste tourspel
+Klopt voor geen meter, mogelijk niet eens een...
+Centimeter!
+Code example below
+"""
+
+#het lijkt er op dat de waarde punten verkeerd is.
+Punten = "te laag voor mijn team"
+Lijst = "raar"
+
+
+while True:
+    if (Punten == "te laag voor mijn team"):
+        Lijst = "niet correct"
+        print (Lijst)
+    
+    elif (Punten == "precies genoeg om op plek 13 te komen"):
+        Lijst = "superieure"
+        print (Lijst)
+    else:
+        Lijst = "correct"
+        print (Lijst)
+    
+    
